4fe88b02240c374bf1dda51888f7b7b913a20528
harmfuly <harmfuly7@gmail.com>
2024-10-21 16:21:26 +0300

    fix: fixed bug in snapshort testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -8,13 +8,12 @@ describe('generateLinkedList', () => {
 
     expect(linkedList).not.toBeNull();
 
-    // Проверяем, что значения узлов связного списка соответствуют ожидаемым
     expect(linkedList?.value).toBe(1);
     expect(linkedList?.next?.value).toBe(2);
     expect(linkedList?.next?.next?.value).toBe(3);
 
-    // Последний узел списка должен иметь null в поле next, чтобы указать на конец списка
-    expect(linkedList?.next?.next?.next).toBeNull();
+    expect(linkedList?.next?.next?.next?.value).toBeNull();
+    expect(linkedList?.next?.next?.next?.next).toBeNull();
   });
 
   // Check match by comparison with snapshot
